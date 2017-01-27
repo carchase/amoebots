3dab998ac3f147879d0f3d41c0a24c94d99653f4
Carter Chase <c.chase8@gmail.com>
2017-01-27 09:58:47 -0500

    cleaned up source directory

--- source/webots/smores_robot/controllers/test/build/release/test.d
+++
@@ -1,5 +0,0 @@
-build/release/test.o: test.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h

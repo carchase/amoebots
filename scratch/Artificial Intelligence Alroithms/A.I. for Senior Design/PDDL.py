576c2b6ab0b4aa2e3074ec69c3057c724c171ff4
Ben Smith <bsbendsmith2@gmail.com>
2017-03-18 14:25:53 -0400

    trying to resolve some errors on upload.

--- scratch/Artificial Intelligence Alroithms/A.I. for Senior Design/PDDL.py
+++ scratch/Artificial Intelligence Alroithms/A.I. for Senior Design/PDDL.py
@@ -27,53 +27,15 @@ def problem(verbose):
     problem = Problem(domain,
                       {'robot': (1, 2),
                        'position': (0, 1), },
-                      init=(('at', 1, 0, 0), ('at', 2, 1, 1),),
+                      init=(('inc', 0, 1), ('dec', 0, 1), ('at', 1, 0, 0), ('at', 2, 1, 1),),
                       goal=(('at', 1, 0, 1), ('at', 2, 1, 0),))
 
-#     plan = planner(problem, verbose=verbose)
-#     if plan is None:
-#         print('No Plan!')
-#     else:
-#         for action in plan:
-#             print(action)
-#
-# if __name__ == '__main__':
-#     from optparse import OptionParser
-#
-#     parser = OptionParser(usage="Usage: %prog [options]")
-#     parser.add_option('-q', '--quiet',
-#                       action='store_false', dest='verbose', default=True,
-#                       help="don't print statistics to stdout")
-#
-#     # Parse arguments
-#     opts, args = parser.parse_args()
-#     problem(opts.verbose)
-
-    def to_coordinates(state):
-        grid = {}
-        for p in state:
-            if p[0] == 'at':
-                grid[p[1]] = (p[2], p[3])
-        return grid
-
-    goal_coords = to_coordinates(problem.goals)
-
-    def manhattan_distance_heuristic(state):
-        state_coords = to_coordinates(state.predicates)
-        dist = 0
-        for k in goal_coords.keys():
-            c1, r1 = goal_coords[k]
-            c2, r2 = state_coords[k]
-            dist += (abs(c1 - c2) + abs(r1 - r2))
-        return dist
-
-    plan = planner(problem, heuristic=manhattan_distance_heuristic, verbose=verbose)
+    plan = planner(problem, verbose=verbose)
     if plan is None:
         print('No Plan!')
     else:
         for action in plan:
             print(action)
-
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -86,3 +48,41 @@ if __name__ == '__main__':
     # Parse arguments
     opts, args = parser.parse_args()
     problem(opts.verbose)
+
+    # def to_coordinates(state):
+    #     grid = {}
+    #     for p in state:
+    #         if p[0] == 'at':
+    #             grid[p[1]] = (p[2], p[3])
+    #     return grid
+    #
+    # goal_coords = to_coordinates(problem.goals)
+    #
+    # def manhattan_distance_heuristic(state):
+    #     state_coords = to_coordinates(state.predicates)
+    #     dist = 0
+    #     for k in goal_coords.keys():
+    #         c1, r1 = goal_coords[k]
+    #         c2, r2 = state_coords[k]
+    #         dist += (abs(c1 - c2) + abs(r1 - r2))
+    #     return dist
+    #
+    # plan = planner(problem, heuristic=manhattan_distance_heuristic, verbose=verbose)
+    # if plan is None:
+    #     print('No Plan!')
+    # else:
+    #     for action in plan:
+    #         print(action)
+
+
+# if __name__ == '__main__':
+#     from optparse import OptionParser
+#
+#     parser = OptionParser(usage="Usage: %prog [options]")
+#     parser.add_option('-q', '--quiet',
+#                       action='store_false', dest='verbose', default=True,
+#                       help="don't print statistics to stdout")
+#
+#     # Parse arguments
+#     opts, args = parser.parse_args()
+#     problem(opts.verbose)
